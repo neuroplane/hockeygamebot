98f2ef0e5bc2087c0ac8922b369c5d7aaaa41ca9
neuroplane <aleksey@boyarkin.com>
2021-11-23 17:44:25 +0300

    commit

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import random
+import statistics
 
-blueteam = [5, 5, 5]
-redteam = [5, 5, 5]
+redteam = [5, 8, 8]
+blueteam = [7, 5, 9]
 
+def roll(case):
+    red20 = random.randint(1, 20)
+    blue20 = random.randint(1, 20)
+    red = statistics.mean(redteam)*red20
+    blue = statistics.mean(blueteam)*blue20
+    print("RED THROW: " + str(red20) + ", BLUE THROW: " + str(blue20))
+    print("RED " + str(round(red, 0)) + " : " + str(round(blue, 0)) + " BLUE")
+    if red > blue:
+        print("RED WIN A " + case + "!")
+    elif blue > red:
+        print("BLUE WIN A " + case + "!")
+    else:
+        print("SUDDEN DEATH!")
 
 def puckdrop():
     print("TIME FOR A PUCKDROP. ROLL A DICE!")
-    red20 = random.randint(1, 20)
-    blue20 = random.randint(1, 20)
+    roll("PUCKDROP")
+
+
 
 puckdrop()
