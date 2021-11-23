909d74a63857267ba39345d50fe7a6442abeba9b
neuroplane <aleksey@boyarkin.com>
2021-11-23 17:22:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+blueteam = [5, 5, 5]
+redteam = [5, 5, 5]
+
+
+def puckdrop():
+    print("TIME FOR A PUCKDROP. ROLL A DICE!")
+    red20 = random.randint(1, 20)
+    blue20 = random.randint(1, 20)
+
+puckdrop()
