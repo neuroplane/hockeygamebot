4408bac5eb81c50f616360dc636a9765e652db1a
neuroplane <aleksey@boyarkin.com>
2021-11-24 15:24:18 +0300

    commit

--- main.py
+++ main.py
@@ -1,63 +1,63 @@
 import random
 import statistics
+import time
 
 # STRENGTH - AGILITY - ACCURACY - SPEED
 puck = None
-teams = ["RED", "BLUE"]
-redteam = [4, 8, 8, 4]
-blueteam = [7, 5, 9, 3]
-stats = ["STRENGTH", "AGILITY", "ACCURACY", "SPEED"]
+teams = [{"RED": {"strength": 4, "agility": 8, "accuracy": 8, "speed": 4}},
+         {"BLUE": {"strength": 7, "agility": 5, "accuracy": 9, "speed": 3}}]
+redteam = ["RED", 5, 7, 7, 4]
+blueteam = ["BLUE", 7, 5, 8, 3]
+stats = ["TEAM", "STRENGTH", "AGILITY", "ACCURACY", "SPEED"]
+puck = None
+
+print("TEAM: " + str(redteam[0]) + ", STRENGTH: " + str(redteam[1]) + ", AGILITY: " + str(
+    redteam[2]) + ", ACCURACY: " + str(redteam[3]) + ", SPEED: " + str(redteam[4]))
+print("TEAM: " + str(blueteam[0]) + ", STRENGTH: " + str(blueteam[1]) + ", AGILITY: " + str(
+    blueteam[2]) + ", ACCURACY: " + str(blueteam[3]) + ", SPEED: " + str(blueteam[4]))
 
 
-def chances():
-    for t in range(0, len(teams)):
-        for s in range(0, len(redteam)):
-            print(teams[t] + " " + stats[s] + " " + str(redteam[s]))
+def diceroll(stat):
+    chance = stat * random.randint(1, 6)
+    return chance
 
 
-chances()
+# NAME - STRENGTH - AGILITY - ACCURACY - SPEED
+def attackchance(attacker):
+    attackerchance = diceroll(statistics.mean([attacker[2], attacker[4]]))
+    print(attacker[0] + "ATTACK CHANCE: " + str(attackerchance))
 
 
-def roll(case, redstat, bluestat):
-    red20 = random.randint(1, 10)
-    blue20 = random.randint(1, 10)
-    red = redstat * red20
-    blue = bluestat * blue20
-    # blue = statistics.mean(blueteam)*blue20
-    while red20 == blue20:
-        print("REROLL")
-        red20 = random.randint(1, 6)
-        blue20 = random.randint(1, 6)
-        red = redstat * red20
-        blue = bluestat * blue20
-    print("RED THROW: " + str(red20) + ", BLUE THROW: " + str(blue20))
-    print("RED " + str(round(red, 0)) + " : " + str(round(blue, 0)) + " BLUE")
-    if red > blue:
-        print("RED WIN " + case + "!")
-        attack("RED")
-        puck = teams[0]
-    elif blue > red:
-        print("BLUE WIN " + case + "!")
-        attack("BLUE")
-        puck = teams[1]
-    else:
-        print("SUDDEN DEATH!")
+def defencechance(defender):
+    defenderchance = diceroll(defender[1])
+    print(defender[0] + " DEFENCE CHANCE: " + str(defenderchance))
+
+
+def shootingchance(shooter):
+    shooterchance = diceroll(statistics.mean([shooter[1], shooter[3]]))
+    print(shooter[0] + " SHOT CHANCE: " + str(shooterchance))
+
+
+def savechance(goalie):
+    goaliechance = diceroll(statistics.mean([goalie[2], goalie[4]]))
+    print(goalie[0] + " SAVE CHANCE: " + str(goaliechance))
 
 
 def puckdrop():
-    # ОБЯЗАТЕЛЬНО СБРОСИТЬ АТАКУ-ЗАЩИТУ ЕСЛИ БЫЛ ЗАБИТ ГОЛ
-    print("TIME FOR A PUCKDROP!")
-    print("RED STRENGTH: " + str(redteam[0]) + ", BLUE STRENGTH: " + str(blueteam[0]))
-    roll("PUCKDROP", redteam[0], blueteam[0])
+    print("PUCK DROP")
 
 
-def attack(attacker):
-    print("NOW " + attacker + " IS ON THE ATTACK!")
+def encounter():
+    print("encounter")
 
 
-def shooting():
-    print("ENCOUNTER")
-    # ОБЯЗАТЕЛЬНО СБРОСИТЬ АТАКУ-ЗАЩИТУ ЕСЛИ БЫЛ ЗАБИТ ГОЛ
+def shot():
+    print("SHOT")
 
 
-puckdrop()
+print("ENCOUNTER")
+attackchance(blueteam)
+defencechance(redteam)
+print("==========\nSHOOTING")
+shootingchance(blueteam)
+savechance(redteam)
